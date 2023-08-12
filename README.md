4612dc9245f1c98f4ffa1911f24a6412a12a08f8
luhao <luhao@flashhold.com>
2023-08-12 11:35:44 +0800

    print resource

---
+++ README.md
@@ -0,0 +1,21 @@
+# Example of Custom Maven Plugin
+
+## use example as local 
+
+build and install the plugin project
+```shell
+mvn install
+```
+
+run plugin goal "hello"
+```shell
+mvn org.example:example-maven-plugin:hello
+```
+
+run usage example
+```shell
+mvn package -f usage-example/pom.xml
+```
+
+
+
--- src/main/java/org/example/ExampleMojo.java
+++ src/main/java/org/example/ExampleMojo.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -7,6 +8,14 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author luhao
@@ -18,12 +27,33 @@ public class ExampleMojo extends AbstractMojo {
     private String welcome;
 
     @Parameter(property = "project", readonly = true)
-    private MavenProject mavenProject;
+    private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        mavenProject.getProperties().put("welcome", welcome);
+        project.getProperties().put("welcome", welcome);
+
+        try {
+            printResources();
+        } catch (Exception e) {
+            throw new MojoExecutionException("read resource dir fail ", e);
+        }
+
         getLog().info(welcome);
 
+    }
+
+    private void printResources() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        List<Resource> resources = project.getResources();
+        for (Resource resource : resources) {
+            String directory = resource.getDirectory();
+            try (Stream<Path> list = Files.list(Paths.get(directory))){
+                List<Path> files = list.collect(Collectors.toList());
+                sb.append(String.format("resource %s (size=%d):\n", directory, files.size()));
+                files.forEach(it -> sb.append("\t\t-").append(it.getFileName()).append("\n"));
+            }
+        }
+        getLog().info(sb);
     }
 }
